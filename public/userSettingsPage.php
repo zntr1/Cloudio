e60c44505d4a454cf2b9b71346aeca0e4298bea9
zntr1 <zesarex@gmail.com>
2018-01-16 13:49:50 +0100

    Einstellungen, Index und CSS überarbeitet

--- public/userSettingsPage.php
+++ public/userSettingsPage.php
@@ -7,110 +7,104 @@ $actualGender = $dbFunction->getGenderById($userData['genderId']);
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <
-    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/contentStyles.css">
+    <link rel="stylesheet" href="../css/styles.css">
+
 </head>
 <body>
 
+<div id="content">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <form action="../script/userSetting.php" method="POST">
+                    <!--// Vorname-->
+                    <div class="form-group">
+                        <label for="settings_firstName">Vorname</label>
+                        <input id="settings_firstName" type="text" name="firstName" class="form-control"
+                               placeholder="Vorname eintragen.."
+                               value="<?php echo $userData['firstName']; ?>"
+                               required>
+                    </div>
 
-<form action="../script/userSetting.php" method="POST" class="form settings">
+                    <!--    // Nachname-->
+                    <div class="form-group">
+                        <label for="settings_surName">Nachname</label>
+                        <input id="settings_surName" type="text" name="lastName" class="form-control"
+                               placeholder="Nachname eintragen.."
+                               value="<?php echo $userData['lastName']; ?>"
+                               required>
+                    </div>
 
-    <div class="form__field">
-        <label for="settings_firstName">
-            <svg class="icon">
-                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#user"></use>
-            </svg>
-            <span class="hidden">Vorname</span></label>
-        <input id="settings_firstName" type="text" name="firstName" class="form__input"
-               value="<?php echo $userData['firstName']; ?>"
-               required>
-    </div>
-    <div class="form__field">
-        <label for="settings_surName">
-            <svg class="icon">
-                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#user"></use>
-            </svg>
-            <span class="hidden">Nachname</span></label>
-        <input id="settings_surName" type="text" name="lastName" class="form__input"
-               value="<?php echo $userData['lastName']; ?>"
-               required>
-    </div>
+                    <!--   // Passwort-->
+                    <div class="form-group">
+                        <label for="settings_old_password">Aktuelles Passwort</label>
+                        <input id="settings_old_password" type="password" name="oldPassword" class="form-control"
+                               placeholder="Aktuelles Passwort.."
+                               value="<?php echo 'noch nichts implemented??' ?>" required>
+                    </div>
 
-    <div class="form__field">
-        <label for="settings_new_password">
-            <svg class="icon">
-                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#lock"></use>
-            </svg>
-            <span class="hidden">Passwort</span></label>
-        <input id="settings_new_password" type="password" name="newPassword" class="form__input"
-               placeholder="Neues Passwort">
-    </div>
+                    <!--   // Passwort-->
+                    <div class="form-group">
+                        <label for="settings_new_password">Neues Passwort</label>
+                        <input id="settings_new_password" type="password" name="newPassword" class="form-control"
+                               placeholder="Passwort ändern.."
+                               value="<?php echo 'noch nichts implemented??' ?>" required>
+                    </div>
 
-    <div class="form__field">
-        <label for="settings_password2">
-            <svg class="icon">
-                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#lock"></use>
-            </svg>
-            <span class="hidden">Neues Passwort bestätigen</span></label>
-        <input id="registration_password2" type="password" name="submitNewPassword" class="form__input"
-               placeholder="Neues Passwort bestätigen">
-    </div>
-    <div class="form__field">
-        <label for="settings_email">
-            <svg class="icon">
-                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#user"></use>
-            </svg>
-            <span class="hidden">Email</span></label>
-        <input id="settings_email" name="email" class="form__input" value="<?php echo $userData['email']; ?>" required>
-    </div>
-    <div class="form__field">
-        <label for="settings_plz">
-            <svg class="icon">
-                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#user"></use>
-            </svg>
-            <span class="hidden">Postleitzahl</span></label>
-        <input id="settings_plz" type="text" name="plz" class="form__input"
-               value="<?php echo $userData['postalcode']; ?>" required>
-    </div>
-    <div class="form__field">
-        <label for="settings_address">
-            <svg class="icon">
-                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#user"></use>
-            </svg>
-            <span class="hidden">Adresse</span></label>
-        <input id="settings_address" type="text" name="address" class="form__input"
-               value="<?php echo $userData['address']; ?>"
-               required>
-    </div>
+                    <!--      // Passwort 2-->
+                    <div class="form-group">
+                        <label for="settings_password2">Passwort wiederholen</label>
+                        <input id="settings_password2" type="password" name="submitNewPassword" class="form-control"
+                               placeholder="Passwort wiederholen.."
+                               value="<?php echo 'noch nichts implemented??' ?>" required>
+                    </div>
 
-    <div class="form__field">
-        <label for="registration_gender">
-            <svg class="icon">
-                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#lock"></use>
-            </svg>
-            <span class="hidden">Geschlecht</span></label>
-        <select class="selectpicker" name="gender" title="Geschlecht">
-            <option><?php echo $actualGender ?></option>
-            <?php
-            echo $dbFunction->getGenderData($actualGender);
-            ?>
-        </select>
-    </div>
+                    <!--     Email-->
+                    <div class="form-group">
+                        <label for="settings_email">Email</label>
+                        <input id="settings_email" type="email" name="email" class="form-control"
+                               placeholder="Email ändern"
+                               value="<?php echo $userData['email']; ?>" required>
+                    </div>
 
-    <div class="form__field">
-        <label for="settings_old_password">
-            <svg class="icon">
-                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#lock"></use>
-            </svg>
-            <span class="hidden">Passwort</span></label>
-        <input id="settings_old_password" type="password" name="oldPassword" class="form__input"
-               placeholder="Altes Passwort"
-               required>
+
+                    <!--   // Adresse-->
+                    <div class="form-group">
+                        <label for="settings_address">Adresse</label>
+                        <input id="settings_address" type="text" name="address" class="form-control"
+                               placeholder="Adresse"
+                               value="<?php echo $userData['address']; ?>" required>
+                    </div>
+
+                    <!--   // PLZ-->
+                    <div class="form-group">
+                        <label for="settings_plz">Postleitzahl</label>
+                        <input id="settings_plz" type="text" name="plz" class="form-control" placeholder="Postleitzahl"
+                               value="<?php echo $userData['postalcode']; ?>" required>
+                    </div>
+
+                <!-- !!!!!!!!!!!!!!!!!! Gender habe ich rausgelassen, mach aktuellen gender als Option im Picker-->
+
+                    <!--    // Genderpick-->
+                    <div class="form-group">
+                        <label for="registration_gender">Geschlecht</label>
+                        <select id="registration_gender" name="gender" class="form-control"
+                                placeholder="Geschlecht auswählen">
+                            <option selected><?php echo $actualGender ?></option>
+                            <?php
+                            echo $dbFunction->getGenderData($actualGender);
+                            ?>
+                        </select>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Daten speichern</button>
+                </form>
+            </div>
+        </div>
     </div>
-    <div class="form__field">
-        <input type="submit" value="Daten Ändern">
-    </div>
-</form>
+</div>
 
 </body>
 </html>
