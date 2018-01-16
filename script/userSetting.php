4a99d334a0c1057d900b68cd0f6e60b537007ae1
SimKoh <kohlruss.simon@gmail.com>
2018-01-16 11:42:53 +0100

    pw fix

--- script/userSetting.php
+++ script/userSetting.php
@@ -11,6 +11,6 @@ require_once '../script/dbConfig.php';
 if (isset($_POST['firstName']) && isset($_POST['lastName']) && isset($_POST['oldPassword']) && isset($_POST['email']) && isset($_POST['plz']) && isset($_POST['address']) && isset($_POST['gender'])) {
     $dbFunction->updateUserData($_POST['oldPassword'], $_POST['firstName'], $_POST['lastName'], $_POST['email'], $_POST['address'], $_POST['plz'], $_POST['gender']);
 }
-if (isset($_POST['oldPassword']) && isset($_POST['newPassword']) && isset($_POST['submitNewPassword'])) {
+if (isset($_POST['oldPassword']) && isset($_POST['newPassword']) && isset($_POST['submitNewPassword'])&& $_POST['newPassword'] !== '') {
     $dbFunction->updatePassword($_POST['oldPassword'], $_POST['newPassword']);
 }
