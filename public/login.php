ba6d64b12a8f945abf638f47b7ddaf6b1088dd5f
zntr1 <zesarex@gmail.com>
2018-01-18 13:18:24 +0100

    CSS angepasst, Seiten hinzugefügt, Dinge gefixt, Dinge gemacht, uff ich bin kaputt

--- public/login.php
+++ public/login.php
@@ -8,7 +8,7 @@
 
 require_once '../script/dbConfig.php';
 
-    $userNamePasswordError = "";
+$userNamePasswordError = "";
 if (isset($_POST['username']) && isset($_POST['password'])) {
     if ($dbFunction->userLogin($_POST['username'], $_POST['password'])) {
         header("location: ../public/index.html");
@@ -26,47 +26,53 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
     <title>Login</title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
 
-    <link rel="stylesheet" href="../css/contentStyles.css">
-    <link rel="stylesheet" href="../css/styles.css">
-    <link rel="stylesheet" href="../css/login.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/login.css">
+
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script type="text/javascript"
+            src="https://cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/jquery.form-validator.min.js"></script>
 
 </head>
 
 <body>
 <body class="align">
+<div class="container">
+    <div class="row">
+        <div class="registrationDiv">
+            <H5 class="text--center" style="font-size: 40px; margin: 0;"> CLOUDIO LOGO </H5>
+            <form action="login.php" method="POST" class="form login" id="loginForm">
+                <div class="input-group">
+                    <span class="input-group-addon"><span class="fa fa-user fa-lg"></span></span>
+                    <input id="login__username" type="text" name="username" class="form-control"
+                           placeholder="Benutzername">
+                </div>
 
-<H5 style="font-size: 40px; margin: 0;"> CLOUDIO LOGO </H5>
-<span><?php echo $userNamePasswordError;?></span>
-<form action="login.php" method="POST" class="form login" id="loginForm">
+                <div class="input-group">
+                    <span class="input-group-addon"><span class=" fa fa-lock fa-lg"></span></span>
+                    <input id="login__password" type="password" name="password" class="form-control"
+                           placeholder="Passwort">
+                </div>
+                <span class="text--center long" style="color:red;"><?php echo $userNamePasswordError; ?></span>
 
-    <div class="form__field">
-        <label for="login__username">
-            <i class="fa fa-user fa-lg"></i>
-        </label>
-        <input id="login__username" type="text" name="username" class="form__input" placeholder="Benutzername">
-    </div>
+                <div class="unten">
+                    <div class="input-group">
+                        <input type="submit" value="Einloggen!">
+                    </div>
+                    <p class="text--center">Noch keinen Account? &nbsp;&nbsp;<a style="color: orange;"
+                                                                                href="../public/register.php">Registriere
+                            dich
+                            kostenlos! &nbsp;<i class="fa fa-sign-in fa-lg"></i> </a>
 
-    <div class="form__field">
-        <label for="login__password">
-            <i class="fa fa-lock fa-lg"></i>
-        </label>
-        <input id="login__password" type="password" name="password" class="form__input" placeholder="Passwort">
-    </div>
-
-
-    <div class="unten">
-        <div class="form__field">
-            <input type="submit" value="Einloggen!">
+                    </p>
+                </div>
+            </form>
         </div>
-        <p class="text--center">Noch keinen Account? &nbsp;&nbsp;<a style="color: orange;"
-                                                                    href="../public/register.php">Registriere dich
-                kostenlos! &nbsp;<i class="fa fa-sign-in fa-lg"></i> </a>
-
-        </p>
     </div>
-</form>
-
+</div>
 <script src="https://cdn.jsdelivr.net/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/jquery.validation/1.15.1/jquery.validate.min.js"></script>
 <script src="js/validate-login.js"></script>
