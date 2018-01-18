b96966d47d8d7591543946af0e4ddb550bc36ce6
SimKoh <kohlruss.simon@gmail.com>
2018-01-18 13:44:49 +0100

    Dummy Daten

--- script/dbFunction.php
+++ script/dbFunction.php
@@ -36,6 +36,7 @@ class dbFunction
             $date_of_expiry = time() + 3600 * 48;
             setcookie("username", "$username", $date_of_expiry, "/");
             setcookie("sessionId", "$sessionId", $date_of_expiry, "/");
+            header("location: ../public/index.html");
             return true;
         } else {
             return false;
